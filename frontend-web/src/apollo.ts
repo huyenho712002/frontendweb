5cc71ef4d2d4c4518f8a8984bfeff3d9702abd2a
huyenho712002 <hohuyen712002@gmail.com>
2024-05-26 08:56:24 +0700

    Your commit message here

---
+++ frontend-web/src/apollo.ts
@@ -0,0 +1,82 @@
+import {
+  split,
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+  makeVar,
+} from "@apollo/client";
+import { LOCALSTORAGE_TOKEN } from "./utils/constants";
+import { setContext } from "@apollo/client/link/context";
+import { WebSocketLink } from "@apollo/client/link/ws";
+import { getMainDefinition } from "@apollo/client/utilities";
+
+const token = localStorage.getItem(LOCALSTORAGE_TOKEN);
+export const isLoggedInVar = makeVar(Boolean(token));
+export const authTokenVar = makeVar(token);
+
+const wsLink = new WebSocketLink({
+  uri:
+    process.env.NODE_ENV === "development"
+      ? "ws://localhost:4000/graphql"
+      : "wss://just-eats-api.herokuapp.com/graphql",
+  options: {
+    reconnect: false,
+    connectionParams: {
+      "X-JWT": authTokenVar() || "",
+    },
+  },
+});
+
+const httpLink = createHttpLink({
+  uri:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:4000/graphql"
+      : "https://just-eats-api.herokuapp.com/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      "X-JWT": authTokenVar() || "",
+    },
+  };
+});
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    );
+  },
+  wsLink,
+  authLink.concat(httpLink)
+);
+
+export const client = new ApolloClient({
+  link: splitLink,
+  headers: {
+    "X-JWT": authTokenVar()!,
+  },
+  cache: new InMemoryCache({
+    // local only fields(App Glbal state)
+    typePolicies: {
+      // Query: {
+      //   fields: {
+      //     isLoggedIn: {
+      //       read() {
+      //         return isLoggedInVar();
+      //       },
+      //     },
+      //     token: {
+      //       read() {
+      //         return authTokenVar();
+      //       },
+      //     },
+      //   },
+      // },
+    },
+  }),
+});
